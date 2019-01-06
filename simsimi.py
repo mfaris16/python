c309d90bbbb1cada7642db1e634ba4f4d33ca597
Muhammad Faris Adi Prabowo <40176160+mfaris16@users.noreply.github.com>
2019-01-06 22:00:14 +0700

    Update simsimi.py

--- simsimi.py
+++ simsimi.py
@@ -5,6 +5,6 @@ print ("Silahkan kirim pesan!")
 while(True):
     pesan = input("Anda: ")
     with urllib.request.urlopen("http://sandbox.api.simsimi.com/request.p?key=dd01228d-7068-4d34-a2ea-668fe42560c8&lc=id&ft=1.0&text=%s" % pesan) as url:
-    data = json.loads(url.read())
+        data = json.loads(url.read())
 
     print ("simsimi: %s" % data['response'])
